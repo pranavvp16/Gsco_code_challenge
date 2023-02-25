094b3c6c50526928205bddb2b14bd979724fe5df
Pranav <pranavprajapati586@gmail.com>
2023-02-26 00:25:14 +0530

    Added css and js

---
+++ script.js
@@ -0,0 +1,19 @@
+let dataCollectorButtons = document.querySelectorAll('button.dataCollector');
+for (let i = 0; i < dataCollectorButtons.length; i++) {
+  dataCollectorButtons[i].addEventListener('mousedown', gatherDataForClass);
+  dataCollectorButtons[i].addEventListener('mouseup', gatherDataForClass);
+  // Populate the human readable names for classes.
+  CLASS_NAMES.push(dataCollectorButtons[i].getAttribute('data-name'));
+}
+
+
+function gatherDataForClass() {
+  // TODO: Fill this out later in the codelab!
+}
+let mobilenet = undefined;
+let gatherDataState = STOP_DATA_GATHER;
+let videoPlaying = false;
+let trainingDataInputs = [];
+let trainingDataOutputs = [];
+let examplesCount = [];
+let predict = false;
